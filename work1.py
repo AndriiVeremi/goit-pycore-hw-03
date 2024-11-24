9c2a4b241ab09b44b481f66bfa131bae3deeb269
Andrii Veremii <114438382+AndriiVeremi@users.noreply.github.com>
2024-11-24 12:06:02 +0100

    add

--- work1.py
+++ work1.py
@@ -1,9 +1,13 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-   current = datetime.now()
-   given_date = datetime.strptime(date, "%Y-%m-%d")
-   delta = current - given_date
-   return delta.days
-
+   try:
+      current = datetime.now()
+      given_date = datetime.strptime(date, "%Y-%m-%d")
+      delta = current - given_date
+      return delta.days
+   except ValueError:
+      print("Неправильний формат дати. Використовуйте 'РРРР-ММ-ДД'.")
+      
+      
 print(get_days_from_today("2020-10-09"))
--- work2.py
+++ work2.py
@@ -5,6 +5,12 @@ def get_numbers_ticket(min, max, quantity):
     q = quantity
     list = []
     
+    if not (1 <= min <= 1000 and 1 <= max <= 1000):
+        return "Помилка: min і max повинні бути між 1 та 1000."
+    
+    if quantity > (max - min + 1):
+        return "Помилка: quantity перевищує кількість можливих чисел у заданому діапазоні."
+    
     while q != 0:      
         num = random.randint(min, max)
         list.append(num)
@@ -13,5 +19,7 @@ def get_numbers_ticket(min, max, quantity):
     return list
 
 lottery_numbers = get_numbers_ticket(1, 49, 6)
+#lottery_numbers = get_numbers_ticket(1, 49, 600)
+#lottery_numbers = get_numbers_ticket(0, 49, 6)
 print("Ваші лотерейні числа:", lottery_numbers)
 
